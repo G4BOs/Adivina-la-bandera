4e7f5913d2701f1f83326e6ec26bf4e1a899df7e
G4BOs <108361351+G4BOs@users.noreply.github.com>
2025-11-25 12:11:32 -0300

    Actualizacion del Script
    v1.5

--- static/js/script.js
+++ static/js/script.js
@@ -1,47 +1,32 @@
+
 const socket = io();
 
-
 const div_bandera = document.getElementById('bandera');
-const btnCambiar = document.getElementById('btnCambiar');
-btnCambiar.addEventListener('click',cambiarBandera);
 
-const input_usr = document.getElementById('userinput');
-const submit = document.getElementById('submit');
-const resultado_txt = document.getElementById('resultado');
+const resultado_txt = document.getElementById('resultadoo');
 
-submit.addEventListener('click',()=>{
-    if (input_usr.value.toLowerCase() == pais_a_adivinar['nombre'].toLowerCase()){
-        resultado_txt.style.color = 'green';
-        resultado_txt.innerHTML = 'CORRECTO BOBO';
-        resultado_decir(input_usr.value.toLowerCase())
-    }
-
-    else{
-        resultado_txt.style.color = 'red';
-        resultado_txt.innerHTML = 'INCORRECTO BOBO'
-    }
-});
-
+var en_juego = true
 
 
 var pais_a_adivinar = {};
+var opciones = [];
 var registrado = false;
 
 
 
-socket.on('set_contry',function(data){
-    console.log(data.nombre);
-    pais_a_adivinar = data;
-    cambiar_bandera(pais_a_adivinar['imagen'])
-
+socket.on('set_opciones',function(data){
+    pais_a_adivinar = data.correcto;
+    opciones = data.opciones;
+    cambiar_bandera(pais_a_adivinar['imagen']);
+    cargar_opciones()
 });
 
 socket.on('decir_resultado',function(data){
-    resultado_txt.innerHTML = data
-})
+    cambiarBandera();
+    en_juego = true
+});
 
-;
-
+//Recibe el pais a adivinar//
 socket.on('decir_pais',function(data){
     pais_a_adivinar = data
     cambiar_bandera(pais_a_adivinar['imagen'])
@@ -63,9 +48,50 @@ function cambiar_bandera(pais){
 };
 
 function cambiarBandera(){
-    socket.emit('cambiar_pais')
+    socket.emit('cambiar_pais');
 };
 
-function resultado_decir(input){
-    socket.emit('resultado', input)
-}
+function resultado_decir(entrada){
+    socket.emit('resultado', entrada);
+};
+
+
+
+// CARGAR OPCIONES//
+
+const contenedor_de_opciones = document.getElementById('opciones');
+
+function cargar_opciones(){
+    contenedor_de_opciones.replaceChildren();
+    for (let pais in opciones){
+        console.log(opciones[pais]);
+        const opcion = document.createElement("div");
+        opcion.className =  'opcion';
+        opcion.id = `opcion_${pais}`;
+        opcion.innerHTML = `${opciones[pais]}`;
+        contenedor_de_opciones.appendChild(opcion);
+        opcion.addEventListener('click',function(){
+            if (opcion.innerHTML == pais_a_adivinar.nombre && en_juego)  {
+                resultado_txt.innerHTML = "CORRECTO";
+                opcion.style.backgroundColor = 'rgba(10,100,20,0.6)';
+                resultado_decir(opcion.innerHTML)
+                ;
+    }
+        else{resultado_txt.innerHTML = "INCORRECTO";
+            opcion.style.backgroundColor = 'rgba(100,10,20,0.6)';
+            en_juego = false
+
+        }
+        });
+    }
+    opciones = []
+        ;
+    
+};
+
+cargar_opciones();
+
+
+
+
+
