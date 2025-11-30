8a4ac0a482b3af303b8da02da275994e6903c7e6
G4BOs <108361351+G4BOs@users.noreply.github.com>
2025-11-30 12:05:13 -0300

    Actualizacion de Scripts

--- static/js/script.js
+++ static/js/script.js
@@ -10,21 +10,37 @@ var en_juego = true
 
 var pais_a_adivinar = {};
 var opciones = [];
-var registrado = false;
 
+var puntuaciones = {};
+
+var registrado = false;
+var mi_id_web = '';
+var mi_numero = 0;
+var betado = false;
 
 
 socket.on('set_opciones',function(data){
     pais_a_adivinar = data.correcto;
     opciones = data.opciones;
+    puntuaciones = data.puntuacion;
+    cargar_puntuacion()
     cambiar_bandera(pais_a_adivinar['imagen']);
     cargar_opciones()
 });
 
 socket.on('decir_resultado',function(data){
     cambiarBandera();
-    en_juego = true
+    en_juego = true;
+    puntuaciones = data;
+    cargar_puntuacion();
 });
+socket.on('mi_id',function(data){
+    mi_id_web = data.id;
+    betado = data.betado;
+    if (data.mi_numero){mi_numero = data.mi_numero;};
+    
+})
+;
 
 //Recibe el pais a adivinar//
 socket.on('decir_pais',function(data){
@@ -52,8 +68,10 @@ function cambiarBandera(){
 };
 
 function resultado_decir(entrada){
-    socket.emit('resultado', entrada);
+    console.log(mi_numero);
+    socket.emit('resultado', {'entrada':entrada,'id':mi_id_web, 'jugador':mi_numero}  );
 };
+
 
 
 
@@ -71,7 +89,7 @@ function cargar_opciones(){
         opcion.innerHTML = `${opciones[pais]}`;
         contenedor_de_opciones.appendChild(opcion);
         opcion.addEventListener('click',function(){
-            if (opcion.innerHTML == pais_a_adivinar.nombre && en_juego)  {
+            if (opcion.innerHTML == pais_a_adivinar.nombre && !betado)  {
                 resultado_txt.innerHTML = "CORRECTO";
                 opcion.style.backgroundColor = 'rgba(10,100,20,0.6)';
                 resultado_decir(opcion.innerHTML)
@@ -79,7 +97,9 @@ function cargar_opciones(){
     }
         else{resultado_txt.innerHTML = "INCORRECTO";
             opcion.style.backgroundColor = 'rgba(100,10,20,0.6)';
-            en_juego = false
+            resultado_decir(opcion.innerHTML);
+            en_juego = false;
+            
 
         }
         });
@@ -91,6 +111,24 @@ function cargar_opciones(){
 
 cargar_opciones();
 
+
+const cuadro_de_puntos = document.getElementById('puntos');
+function cargar_puntuacion(){
+    cuadro_de_puntos.replaceChildren();
+    for (let puntos in puntuaciones){
+        console.log(puntuaciones[puntos],puntos);
+
+        var txt_puntos = document.createElement('p');
+        txt_puntos.style.display = 'inline';
+        txt_puntos.className= 'punto';
+        txt_puntos.innerText = `${puntos}: ${puntuaciones[puntos].punto}`
+        cuadro_de_puntos.appendChild(txt_puntos)
+
+        
+    }
+}
+
+cargar_puntuacion();
 
 
 
